cf1f39e4f96b59d7e4d4ec1481c17ad18a3d05ae
dawndelirus <61913528+dawndelirus@users.noreply.github.com>
2021-04-10 01:23:08 +0200

    Ram flee behaviour on player bark

--- GameProject/Assets/Scripts/RamBehaviour.cs
+++ GameProject/Assets/Scripts/RamBehaviour.cs
@@ -2,23 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System;
 
 public class RamBehaviour : MonoBehaviour
 {
     public enum RamState
     {
-        Idle,
-        Wandering,
+        Wander,
+        Flee,
         Rage
     }
 
-    //private RamState _state = RamState.Idle;
+    //private Action OnPlayerBark;
+
+    private RamState _state = RamState.Wander;
     //private Timer _rageTimer = new Timer();
 
     // Movement
-    [SerializeField]
-    private NavMeshAgent _navMesh;
-    private float _walkRadius = 5.0f;
+    [SerializeField] private NavMeshAgent _navMesh;
+    [SerializeField] private float _maxWalkRadius = 5.0f;
+    [SerializeField] private float _minWalkRadius = 1.0f;
     Vector3 _targetPosition;
     private Timer _wanderTimer = new Timer();
 
@@ -26,32 +29,60 @@ public class RamBehaviour : MonoBehaviour
 
     private void Start()
     {
-        //_state = RamState.Wandering;
+        _state = RamState.Wander;
         _wanderTimer.Set(3.0f);
-        SetDestination();
-    }
-
-    private void Do()
-    {
-
+        SetRandomDestination();
     }
 
     private void Update()
     {
-        _wanderTimer.OnPing(Time.deltaTime, SetDestination);
-        Wander();
+        switch (_state)
+        {
+            case RamState.Wander:
+                _wanderTimer.OnPing(Time.deltaTime, SetRandomDestination);
+                Move();
+                break;
+            case RamState.Flee:
+                Move();
+                if (Equals(transform.position.x, _targetPosition.x) && Equals(transform.position.z, _targetPosition.z))
+                {
+                    _state = RamState.Wander;
+                }
+                break;
+            case RamState.Rage:
+                break;
+            default:
+                break;
+        }
     }
 
-    private void Wander()
+    private void Move()
     {
         _navMesh.SetDestination(_targetPosition);
     }
 
-    private void SetDestination()
+    private void SetRandomDestination()
     {
-        Vector3 randomDirection = Random.insideUnitSphere * _walkRadius;
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * UnityEngine.Random.Range(_minWalkRadius, _maxWalkRadius);
         randomDirection += transform.position;
-        NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, _walkRadius, 1);
+        NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, _maxWalkRadius, 1);
+        if (hit.hit)
+        {
+            _targetPosition = hit.position;
+        }
+    }
+
+    public void RecieveBark(float barkPower, Vector3 barkPosition)
+    {
+        _state = RamState.Flee;
+
+        float randomnessFactor = 0.5f;
+        Vector3 dirRandom = UnityEngine.Random.insideUnitSphere * randomnessFactor;
+
+        Vector3 direction = (transform.position - barkPosition + dirRandom).normalized;
+        direction += transform.position;
+
+        NavMesh.SamplePosition(direction, out NavMeshHit hit, barkPower, 1);
         if (hit.hit)
         {
             _targetPosition = hit.position;
