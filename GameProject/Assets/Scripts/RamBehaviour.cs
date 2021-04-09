29f14f0826e02f84a79e6ad5c664b48738bd1b67
Kairoz <52052044+Kair0z@users.noreply.github.com>
2021-04-09 21:26:19 +0200

    Scripts lol

--- GameProject/Assets/Scripts/RamBehaviour.cs
+++ GameProject/Assets/Scripts/RamBehaviour.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class RamBehaviour : MonoBehaviour
 {
+    public enum RamState
+    {
+        Idle,
+        Wandering,
+        Rage
+    }
 
+    private RamState _state = RamState.Idle;
+    private Timer _testTimer = new Timer();
 
+    private void Start()
+    {
+        _state = RamState.Wandering;
+    }
 }
