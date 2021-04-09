2c7e689174f48872c8cc9720d303768036109ff4
PepijnVerhaert <pepijn.verhaert@student.howest.be>
2021-04-10 00:11:34 +0200

    GRRRRR

--- GameProject/Assets/Scripts/PreyBehaviour.cs
+++ GameProject/Assets/Scripts/PreyBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PreyBehaviour : MonoBehaviour
 {
@@ -10,13 +11,19 @@ public class PreyBehaviour : MonoBehaviour
         Fleeing
     }
 
+    [Header("Wander")]
+    [SerializeField] private float wanderRadius = 5.0f;
+    [SerializeField] private float wanderTime = 3.0f;
+
+    [SerializeField] private NavMeshAgent navMeshAgentComp = null;
+
     private PreyState _state = PreyState.Wandering;
     private Timer _wanderTimer = new Timer();
 
     private void Start()
     {
         _state = PreyState.Wandering;
-        _wanderTimer.Set(5);
+        _wanderTimer.Set(wanderTime);
     }
 
     private void Update()
@@ -25,6 +32,18 @@ public class PreyBehaviour : MonoBehaviour
     }
 
     private void DetermineNewWanderTarget()
+    {
+        Vector2 point = Random.insideUnitCircle.normalized * Random.Range(0, wanderRadius);
+        navMeshAgentComp.SetDestination(new Vector3(point.x, 0f, point.y));
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, wanderRadius);
+    }
+
+    public void RecieveBark(float barkPower, Vector3 barkPosition)
     {
 
     }
