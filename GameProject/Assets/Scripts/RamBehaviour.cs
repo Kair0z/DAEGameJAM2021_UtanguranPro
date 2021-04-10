bb9eab898caa242ccdb48c332b34900b4fdea414
dawndelirus <61913528+dawndelirus@users.noreply.github.com>
2021-04-10 02:03:16 +0200

    Rage behaviour

--- GameProject/Assets/Scripts/RamBehaviour.cs
+++ GameProject/Assets/Scripts/RamBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using System;
+//using System;
 
 public class RamBehaviour : MonoBehaviour
 {
@@ -16,21 +16,31 @@ public class RamBehaviour : MonoBehaviour
     //private Action OnPlayerBark;
 
     private RamState _state = RamState.Wander;
-    //private Timer _rageTimer = new Timer();
 
-    // Movement
+    [Header("Movement")]
     [SerializeField] private NavMeshAgent _navMesh;
-    [SerializeField] private float _maxWalkRadius = 5.0f;
-    [SerializeField] private float _minWalkRadius = 1.0f;
     Vector3 _targetPosition;
+
+    [Header("Wander")]
+    [SerializeField] private float _maxWanderRadius = 5.0f;
+    [SerializeField] private float _minWanderRadius = 1.0f;
+    [SerializeField] private float _wanderSpeed = 3.0f;
     private Timer _wanderTimer = new Timer();
 
-    //private float _rageBar = 0;
+    [Header("Flee")]
+    [SerializeField] private float _fleeSpeed = 10.0f;
+    [SerializeField] private float _fleeMultiplier = 5.0f;
+
+    [Header("Rage")]
+    [SerializeField] private float _chargeSpeed = 10.0f;
+    [SerializeField] private float _rageIncreaseAmount = 10.0f;
+    private float _rageBar = 0;
 
     private void Start()
     {
         _state = RamState.Wander;
         _wanderTimer.Set(3.0f);
+        _navMesh.speed = _wanderSpeed;
         SetRandomDestination();
     }
 
@@ -39,17 +49,13 @@ public class RamBehaviour : MonoBehaviour
         switch (_state)
         {
             case RamState.Wander:
-                _wanderTimer.OnPing(Time.deltaTime, SetRandomDestination);
-                Move();
+                Wander();
                 break;
             case RamState.Flee:
-                Move();
-                if (Equals(transform.position.x, _targetPosition.x) && Equals(transform.position.z, _targetPosition.z))
-                {
-                    _state = RamState.Wander;
-                }
+                Flee();
                 break;
             case RamState.Rage:
+                Rage();
                 break;
             default:
                 break;
@@ -63,9 +69,9 @@ public class RamBehaviour : MonoBehaviour
 
     private void SetRandomDestination()
     {
-        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * UnityEngine.Random.Range(_minWalkRadius, _maxWalkRadius);
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * UnityEngine.Random.Range(_minWanderRadius, _maxWanderRadius);
         randomDirection += transform.position;
-        NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, _maxWalkRadius, 1);
+        NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, _maxWanderRadius, 1);
         if (hit.hit)
         {
             _targetPosition = hit.position;
@@ -76,7 +82,9 @@ public class RamBehaviour : MonoBehaviour
     {
         _state = RamState.Flee;
 
-        float randomnessFactor = 0.5f;
+        barkPower *= _fleeMultiplier;
+
+        float randomnessFactor = 1.0f;
         Vector3 dirRandom = UnityEngine.Random.insideUnitSphere * randomnessFactor;
 
         Vector3 direction = (transform.position - barkPosition + dirRandom).normalized;
@@ -89,19 +97,51 @@ public class RamBehaviour : MonoBehaviour
         }
     }
 
+    private void Wander()
+    {
+        _navMesh.speed = _wanderSpeed;
+        _wanderTimer.OnPing(Time.deltaTime, SetRandomDestination);
+        Move();
+    }
+
     private void Flee()
     {
+        FillRageBar();
+        _navMesh.speed = _fleeSpeed;
+        Move();
+        if (Equals(transform.position.x, _targetPosition.x) && Equals(transform.position.z, _targetPosition.z))
+        {
+            _state = RamState.Wander;
+        }
+    }
 
+    private void Rage()
+    {
+        _rageBar -= 0.2f;
+        Charge();
+
+        if (_rageBar <= 0.0f)
+        {
+            _state = RamState.Wander;
+            _rageBar = 0.0f;
+        }
     }
 
     private void Charge()
     {
-
+        _navMesh.speed = _chargeSpeed;
+        // set target location to grain field
+        Move();
     }
 
-    private void Eat()
+    private void FillRageBar()
     {
-
+        _rageBar += _rageIncreaseAmount;
+        Debug.Log(_rageBar);
+        if (Equals(_rageBar, 100.0f))
+        {
+            _state = RamState.Rage;
+        }
     }
 
     private void Trample()
